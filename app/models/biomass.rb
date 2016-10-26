4a41bcd03240f4811f1ee061fe93304ec4beecd4
Sean Patchin <smpatchin@wisc.edu>
2016-10-26 13:26:03 -0500

    index shows owned items only

--- app/models/biomass.rb
+++ app/models/biomass.rb
@@ -1,3 +1,7 @@
 class Biomass < ApplicationRecord
   belongs_to :owner, class_name: 'User'
+
+  def self.owned_objects
+    
+  end
 end
--- app/models/biomass_type.rb
+++ app/models/biomass_type.rb
@@ -1,3 +1,7 @@
 class BiomassType < ApplicationRecord
   belongs_to :owner, class_name: 'User'
+
+  def self.owned_objects
+    all.reject { |o| o.owner != current_user }
+  end
 end
