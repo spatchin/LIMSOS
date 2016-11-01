fd277c930f0bcf0dfa96c50534494ca766186387
spatchin <spatchin94@gmail.com>
2016-11-01 13:13:18 -0500

    commiting so i don't mess up the generator file

--- lib/generators/ramodel/ramodel_generator.rb
+++ lib/generators/ramodel/ramodel_generator.rb
@@ -3,34 +3,27 @@ class RamodelGenerator < Rails::Generators::NamedBase
 
   source_root File.expand_path('../templates', __FILE__)
 
-  desc 'Generate a standard model'
   def create_model
     generate 'model', "#{file_name} #{attributes_to_arguments}"
   end
 
-  desc 'Add configuration to rails_admin.rb'
   def add_to_rails_admin_config
-    inject_into_file "#{Rails.root}/config/initializers/rails_admin.rb", after: "# Add new model configurations here\n" do <<-'RUBY'
-      puts "\tconfig.model #{name.camelize} do"
-      puts "\t\towner_config"
-      puts "\tend"
-    RUBY
+    insert_into_file "#{Rails.root}/config/initializers/rails_admin.rb", after: "# Add new model configurations here\n" do
+      ["\tconfig.model #{name.camelize} do",
+      "\t\towner_config",
+      "\tend"].join("\n")
     end
   end
 
-  desc 'Add PaperTrail to model'
   def add_to_cancan
-    inject_into_file "#{Rails.root}/app/models/#{name.underscore}.rb", after: "class #{name.camelize} < ApplicationRecord\n" do <<-'RUBY'
-      puts "\thas_paper_trail"
-    RUBY
+    insert_into_file "#{Rails.root}/app/models/#{name.underscore}.rb", after: "class #{name.camelize} < ApplicationRecord\n" do
+      "\thas_paper_trail"
     end
   end
 
-  desc 'Add Cancan ability to ability.rb'
   def add_to_cancan
-    inject_into_file "#{Rails.root}/app/models/ability.rb", after: "# Add new model permissions here\n" do <<-'RUBY'
-      puts "\t\t\tcan :manage, #{name.camelize}, owner_id: user.id"
-    RUBY
+    insert_into_file "#{Rails.root}/app/models/ability.rb", after: "# Add new model permissions here\n" do
+      "\t\t\tcan :manage, #{name.camelize}, owner_id: user.id"
     end
   end
 
