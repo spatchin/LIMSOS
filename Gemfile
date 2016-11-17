e7b70458cd12f11e943c9a0dc5d5e0ec2d3f56d1
spatchin <spatchin94@gmail.com>
2016-11-17 11:47:12 -0600

    updated listen gem

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ group :development, :test do
 end
 group :development do
   gem 'web-console'
-  gem 'listen', '~> 3.0.5'
+  gem 'listen'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'better_errors'
--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, '/var/www/html/limsos-dev2'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
